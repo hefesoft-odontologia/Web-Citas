293503eb7545d2a8adfd0f6895a5781d74de8a7d
Jose Douglas Ramirez E <chibchombiano26@hotmail.com>
2015-04-29 17:28:40 -0500

    Push Username

--- js/Controllers/prestadorFechas.js
+++ js/Controllers/prestadorFechas.js
@@ -34,6 +34,7 @@ angular.module('starter')
         
         dataTableStorageFactory.saveStorage(data).then(citaSolicitada, error);
         UniversalApps.push(item.email, "Cita solicitada por: " + usuario.email, 0.1);
+        pushFactory.enviarMensajeUsername(item.email, "Cita solicitada por: " + usuario.email);        
         UniversalApps.alert("Cita solicitada en espera de respuesta.", 8)
 
     }
@@ -69,7 +70,7 @@ angular.module('starter')
          //var textoCita = 'Nueva cita solicitada';
         //pushFactory.enviarMensajePlatform(item.email,textoCita, item.platform);
         //emailFactory.enviarEmail(usuario.email, item.email, 'Cita solicitada', textoCita, textoCita);
-        $state.go('app.citasolicitada');
+        $state.go('app.citasolicitada');        
     }
 
 
