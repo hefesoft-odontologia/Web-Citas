60b7f64b3661bd6e03fb70dcc1f92f8868489ba4
Jose Douglas Ramirez E <chibchombiano26@hotmail.com>
2015-04-29 15:55:50 -0500

    Alert

--- js/Controllers/prestadorFechas.js
+++ js/Controllers/prestadorFechas.js
@@ -34,7 +34,7 @@ angular.module('starter')
         
         dataTableStorageFactory.saveStorage(data).then(citaSolicitada, error);
         UniversalApps.push(item.email, "Cita solicitada por: " + usuario.email, 0.1);
-        UniversalApps.toast("Cita solicitada en espera de respuesta.", 8)
+        UniversalApps.alert("Cita solicitada en espera de respuesta.", 8)
 
     }
 
